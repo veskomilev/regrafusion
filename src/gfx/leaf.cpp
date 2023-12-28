f8e118b89f0a962af330b0f776e524ddd88ce58c
Vesko Milev <>
2023-12-28 22:31:02 +0200

    Leaf: Draw the transformation matrix

--- src/gfx/leaf.cpp
+++ src/gfx/leaf.cpp
@@ -21,7 +21,19 @@ void Leaf::draw(std::shared_ptr<QPainter> painter)
         painter->setWorldTransform(matrix_, true);
     }
 
-    // draw matrix here...
+    // in principle the side should be 1, but 1 pixel is too short of a length
+    const uint side = 30;
+
+    // draw the transformation matrix
+    painter->setPen(QColor(128, 128, 128, 64));
+    painter->drawLine(side, 0, side, side);
+    painter->drawLine(0, side, side, side);
+
+    painter->setPen(QColor(255, 0, 0, 64));
+    painter->drawLine(0, side, 0, 0);
+
+    painter->setPen(QColor(0, 0, 255, 64));
+    painter->drawLine(side, 0, 0, 0);
 }
 
 bool Leaf::setTransformationMatrix(QTransform matrix)
