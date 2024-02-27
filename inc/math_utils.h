5e8ecc420c9bb38f06f6ac7b49073ac91b2b43a9
Vesko Milev <>
2024-02-27 16:50:57 +0200

    math: getPointDistance

--- inc/math_utils.h
+++ inc/math_utils.h
@@ -5,6 +5,8 @@
 #ifndef MATH_UTILS_H
 #define MATH_UTILS_H
 
+#include <QPoint>
+#include <QPointF>
 #include <QTransform>
 
 struct TransformationInfo {
@@ -52,6 +54,18 @@ void math_utils_test()
     assert(info.rotation_deg == 70);
     assert(info.scale.rx() == 2.4);
     assert(info.scale.ry() == 2.8);
+}
+
+inline qreal getPointDistance(QPointF p1, QPointF p2)
+{
+    qreal x = p1.rx() - p2.rx();
+    qreal y = p1.ry() - p2.ry();
+    return sqrt(x * x + y * y);
+}
+
+inline qreal getPointDistance(QPoint p1, QPoint p2)
+{
+    return getPointDistance(p1.toPointF(), p2.toPointF());
 }
 
 #endif // MATH_UTILS_H
